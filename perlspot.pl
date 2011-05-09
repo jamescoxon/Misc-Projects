7864233e8e3409e4b0d2b6ace56f53f833a76535
James Coxon <jacoxon@googlemail.com>
2011-05-09 12:25:50 +0100

    fixed

--- perlspot.pl
+++ perlspot.pl
@@ -1,18 +1,19 @@
 
-
 use LWP::UserAgent;
 use Time::Local;
 use POSIX;
 
-$glId = "0VSOSac1275Xczm05gSxTBEcJcPoAf6Ix";
+$glId = "02vhBoDslO1BaEqCUR9sUikleajn0x2u4";
 $count = 0;
 $oldtime = 1301760620;
 $altencoded = 1;
 
-my $ua = LWP::UserAgent->new;
-$ua->timeout(120);
-my $url='http://share.findmespot.com/messageService/guestlinkservlet';
-my $response = $ua->post(
+while (1)
+{
+	my $ua = LWP::UserAgent->new;
+	$ua->timeout(120);
+	my $url='http://share.findmespot.com/messageService/guestlinkservlet';
+	my $response = $ua->post(
 	$url, 
 	[
 	"glId" => $glId, 
@@ -20,38 +21,39 @@ my $response = $ua->post(
 	"linkPw" => "", 
 	"mode" => "none", 
 	"start" => "0", 
-]);
-
-print $response;
-#my $response = $ua->post($request);
-my $content = $response->content();
-print "$content\n";
-
-@splitsections = split(/<message>/, $content);
-
-foreach $sections (@splitsections) {
-	print "$sections\n";
-	@components = split(/<\//, $sections);
-	foreach $subsections (@components) {
-		print "$subsections\n";
-		@subcomp = split(/<latitude>/, $subsections);
-		if ($subcomp[1] ne "") {
-			$latitude = $subcomp[1];
+	]);
+	
+	print $response;
+	#my $response = $ua->post($request);
+	my $content = $response->content();
+	print "$content\n";
+	
+	@splitsections = split(/<message>/, $content);
+	
+	foreach $sections (@splitsections) {
+		print "$sections\n";
+		@components = split(/<\//, $sections);
+		foreach $subsections (@components) {
+			print "$subsections\n";
+			@subcomp = split(/<latitude>/, $subsections);
+			if ($subcomp[1] ne "") {
+				$latitude = $subcomp[1];
+			}
+			print "$subcomp[1]\n";
+			@subcomp = split(/<longitude>/, $subsections);
+			if ($subcomp[1] ne "") {
+				$longitude = $subcomp[1];
+			}
+			print "$subcomp[1]\n";
+			
+			@subcomp = split(/<timeInGMTSecond>/, $subsections);
+			if ($subcomp[1] ne "") {
+				$time = $subcomp[1];
+			}
+			print "$subcomp[1]\n";
 		}
-		print "$subcomp[1]\n";
-		@subcomp = split(/<longitude>/, $subsections);
-		if ($subcomp[1] ne "") {
-			$longitude = $subcomp[1];
-		}
-		print "$subcomp[1]\n";
-		
-		@subcomp = split(/<timeInGMTSecond>/, $subsections);
-		if ($subcomp[1] ne "") {
-			$time = $subcomp[1];
-		}
-		print "$subcomp[1]\n";
 	}
-
+	
 	if ($altencoded) {
 		# Extract encoded coordinates and altitude
 		$latitude  /= 90.0 / 99.0;
@@ -73,17 +75,21 @@ foreach $sections (@splitsections) {
 	}
 	
 	print "Time = $time, Latitude = $latitude, Longitude = $longitude, Altitude = $altitude\n";
-	$count = $count + 1;
 	
-	my $time1 = time;
-	my ($sec, $min, $hour, $day,$month,$year) = (gmtime($time1))[0,1,2,3,4,5,6];
-	
-	$datastring = "KI6YMZ,$count,$hour:$min:$sec,$latitude,$longitude,$altitude,0,0,0";
-	
-	print "$datastring\n";
-	
-	my $rh = new LWP::UserAgent;
-	$rh->timeout(120);
-	my $response = $rh->post( "http://www.robertharrison.org/listen/listen.php", { 'string' => $datastring, 'identity' => "SPOT" } );
-sleep(2);
+	if ($time > $oldtime) {
+		$oldtime = $time;
+		$count = $count + 1;
+		
+		my $time1 = time;
+		my ($sec, $min, $hour, $day,$month,$year) = (gmtime($time1))[0,1,2,3,4,5,6];
+		
+		$datastring = "KI6YMZ,$count,$hour:$min:$sec,$latitude,$longitude,$altitude,0,0,0";
+		
+		print "$datastring\n";
+		
+		my $rh = new LWP::UserAgent;
+		$rh->timeout(120);
+		my $response = $rh->post( "http://www.robertharrison.org/listen/listen.php", { 'string' => $datastring, 'identity' => "SPOT" } );
+	}
+	sleep(60);
 }
